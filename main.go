1189bc2520d918dea364b6dad33c743005caa4e1
lyutvs <lyutvs@gmail.com>
2022-12-12 23:18:40 +0900

    ⚡️ :: main.go

--- main.go
+++ main.go
@@ -2,104 +2,52 @@ package main
 
 import (
 	"fmt"
-	"golang_learn/somting"
-	"strings"
+	"golang_learn/account"
+	"golang_learn/mydict"
 )
 
-type person struct {
-	name    string
-	age     int
-	favFood []string
-}
-
-func lenAndUpper(name string) (int, string) {
-	return len(name), strings.ToUpper(name)
-}
-
-func repeatMe(word ...string) {
-	fmt.Println(word)
-}
-
-func lenAndUppers(name string) (length int, upper string) {
-	defer fmt.Println("bybyebyebeye")
-	length = len(name)
-	upper = strings.ToUpper(name)
-	return
-}
-
-func superAdd(numbers ...int) int {
-	defer fmt.Println("superAdd")
-	for index, num := range numbers {
-		fmt.Println(index, num)
-	}
-	return 1
-}
-
-func loopAdd(numbers ...int) int {
-	defer fmt.Println("loopAdd")
-	total := 0
-	for _, num := range numbers {
-		total += num
-	}
-	return total
-}
-
-func naturalLoop(number ...int) int {
-	for i := 0; i < len(number); i++ {
-		fmt.Println(number[i])
-	}
-	return 1
-}
-
-func canIDrive(age int) bool {
-	switch koreanAge := age + 2; koreanAge {
-	case 10:
-		return false
-	case 18:
-		return true
-	}
-	return false
-}
-
-func canIdDrink(age int) bool {
-	if koreanAge := age + 2; koreanAge < 18 {
-		return false
-	}
-	return true
-}
-
 func main() {
-	fmt.Println(canIDrive(18))
+	account := account.NewAccount("sehyoun")
+	account.Deposit(10)
+	fmt.Println(account.Balance())
 
-	fmt.Println(canIdDrink(16))
+	//err := account.WithDraw(20)
+	//if err != nil {
+	//	log.Fatal(err)
+	//}
 
-	totalLength, upperName := lenAndUpper("hello")
+	fmt.Println(account.Balance(), account.Owner())
 
-	fmt.Println(totalLength, upperName)
+	dictionary := mydict.Dictionary{"first": "First word"}
+	definition, err := dictionary.Search("second")
 
-	totalLengths, upperNames := lenAndUppers("jeonsehyoun")
-	fmt.Println(totalLengths, upperNames)
+	word := "hello"
+	gretingWord := "Greeting"
+	if err != nil {
+		fmt.Println(err)
+	} else {
+		fmt.Println(definition)
+	}
 
-	repeatMe("hello", "hohhooh", "qwdqw", "12343")
-	somting.Bye()
+	errAdd := dictionary.Add(word, gretingWord)
 
-	superAdd(1, 2, 3, 4, 5, 6)
+	if errAdd != nil {
+		fmt.Println(errAdd)
+	}
+	hello, err := dictionary.Search(word)
+	fmt.Println(hello)
 
-	naturalLoop(1, 2, 3, 4, 5, 6, 7, 8, 9)
+	erre := dictionary.Update(";s;s;s;s", "Second")
 
-	result := loopAdd(1, 2, 3, 4, 5, 6, 7, 8, 9)
-	fmt.Println(result)
+	baseWord := "hellow"
 
-	names := []string{"nico", "lynn", "dal"}
-	names = append(names, "flynn")
-	fmt.Println(names)
+	if err != nil {
+		fmt.Println(erre)
+	}
 
-	sehyoun := map[string]string{"name": "sehyoun", "age": "12"}
-	fmt.Println(sehyoun)
+	wordd, _ := dictionary.Search(baseWord)
 
-	favFood := []string{"kimchi", "ramen"}
-	sehyounn := person{"sehyoun", 18, favFood}
-	sehyonType := person{name: "sehyoun", age: 18, favFood: favFood}
-	fmt.Println(sehyounn)
-	fmt.Println(sehyonType)
+	dictionary.Delete(baseWord)
+
+	fmt.Println(wordd)
 }
